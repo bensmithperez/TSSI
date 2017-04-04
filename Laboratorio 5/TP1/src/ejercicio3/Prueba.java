adc3f8eaee1716d4b6cc086a27d92a3aed3fb392
Benjamin Smith <bensmithperez@gmail.com>
2017-04-03 21:25:06 -0300

    Rename Programación Avanzada 1/TP1/src/ejercicio3/Prueba.java to Laboratorio 5/TP1/src/ejercicio3/Prueba.java

--- Laboratorio 5/TP1/src/ejercicio3/Prueba.java
+++ Laboratorio 5/TP1/src/ejercicio3/Prueba.java
@@ -7,8 +7,8 @@ public class Prueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Profesor p1 = new Profesor();
-		Profesor p2 = new Profesor("Matem·tica",2,"Juan",50);
-		Profesor p3 = new Profesor("ProgramaciÛn",10,"Pepe",20);
+		Profesor p2 = new Profesor("Matem√°tica",2,"Juan",50);
+		Profesor p3 = new Profesor("Programaci√≥n",10,"Pepe",20);
 		
 		System.out.println(p1.devuelveDatos());
 		System.out.println(p2.devuelveDatos());
