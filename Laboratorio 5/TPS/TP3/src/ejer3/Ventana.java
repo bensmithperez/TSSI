e5bd27df1ba863a75fdf52622fca202b111a0760
Benjamin Smith <info@benjismithperez.com>
2017-04-18 19:23:20 -0300

    arranco ejer3

--- Laboratorio 5/TPS/TP3/src/ejer3/Ventana.java
+++ Laboratorio 5/TPS/TP3/src/ejer3/Ventana.java
@@ -3,10 +3,22 @@ package ejer3;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.JTextPane;
 
 public class Ventana {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
@@ -36,8 +48,51 @@ public class Ventana {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 350, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNombre = new JLabel("Nombre:");
+		lblNombre.setBounds(54, 34, 116, 14);
+		frame.getContentPane().add(lblNombre);
+		
+		JTextField txtNombre = new JTextField();
+		txtNombre.setColumns(10);
+		txtNombre.setBounds(176, 31, 86, 20);
+		frame.getContentPane().add(txtNombre);
+		
+		JLabel lblPrecio = new JLabel("Precio:");
+		lblPrecio.setBounds(54, 60, 116, 14);
+		frame.getContentPane().add(lblPrecio);
+		
+		JTextField txtPrecio = new JTextField();
+		txtPrecio.setColumns(10);
+		txtPrecio.setBounds(176, 57, 86, 20);
+		frame.getContentPane().add(txtPrecio);
+		
+		JLabel lblStock = new JLabel("Stock:");
+		lblStock.setBounds(54, 86, 116, 14);
+		frame.getContentPane().add(lblStock);
+		
+		JTextField txtStock = new JTextField();
+		txtStock.setColumns(10);
+		txtStock.setBounds(176, 83, 86, 20);
+		frame.getContentPane().add(txtStock);
+		
+		JLabel lblCategoria = new JLabel("Categor\u00EDa:");
+		lblCategoria.setBounds(54, 112, 116, 14);
+		frame.getContentPane().add(lblCategoria);
+		
+		JComboBox<String> cbCategoria = new JComboBox<String>();
+		cbCategoria.setBounds(176, 109, 28, 20);
+		frame.getContentPane().add(cbCategoria);
+		
+		JButton btnGuardar = new JButton("Guardar");
+		btnGuardar.setBounds(176, 160, 86, 23);
+		frame.getContentPane().add(btnGuardar);
+		
+		JTextPane tpDatos = new JTextPane();
+		tpDatos.setBounds(40, 214, 250, 200);
+		frame.getContentPane().add(tpDatos);
 	}
-
 }
