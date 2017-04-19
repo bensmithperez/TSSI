0b6b6ff9f09f1ec9c8d9d878dd683a62f8cd912f
Benjamin Smith <info@benjismithperez.com>
2017-04-18 21:14:27 -0300

    cambios ejer1

--- Laboratorio 5/TPS/TP5/src/ejer1/FormPrincipal.java
+++ Laboratorio 5/TPS/TP5/src/ejer1/FormPrincipal.java
@@ -3,6 +3,9 @@ package ejer1;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JMenu;
 
 public class FormPrincipal {
 
@@ -38,6 +41,24 @@ public class FormPrincipal {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenu mnPersona = new JMenu("Persona");
+		menuBar.add(mnPersona);
+		
+		JMenuItem mntmAgregar = new JMenuItem("Agregar");
+		mnPersona.add(mntmAgregar);
+		
+		JMenuItem mntmModificar = new JMenuItem("Modificar");
+		mnPersona.add(mntmModificar);
+		
+		JMenuItem mntmEliminar = new JMenuItem("Eliminar");
+		mnPersona.add(mntmEliminar);
+		
+		JMenuItem mntmListar = new JMenuItem("Listar");
+		mnPersona.add(mntmListar);
 	}
 
 }
